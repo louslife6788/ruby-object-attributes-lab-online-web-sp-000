b7188829ec67f8824e602b65cfcc3ab5758e6b82
luis leon <leonluis380@yahoo.com>
2021-06-10 21:09:16 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,46 @@
+class Dog
+
+    def name=(lable)
+        @name = lable
+    end
+
+    def name
+        @name
+    end
+
+    def breed=(type)
+        @breed = type
+
+    end
+
+    def breed
+        @breed
+    end
+
+end
+
+
+class Person
+
+
+    def name=(lable)
+        @name = lable
+    end
+
+
+    def name 
+        @name
+
+    end
+
+    def job=(work)
+        @job = work
+    end
+
+    def job 
+        @job
+    end
+
+end
+
+
